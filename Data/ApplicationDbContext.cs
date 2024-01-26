89602936808080772abb4cfb72f7b6eff83faea4
Ellie <112726692+elliezub@users.noreply.github.com>
2024-01-25 23:47:22 -0500

    Connect to database

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,7 +3,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace HabitTracker.Data
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-    {
-    }
+	public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+		}
+
+		public DbSet<TodoItem> TodoItems { get; set; }
+	}
 }
